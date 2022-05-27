f283d78f589ebf719920134e08c3580b6ff9f33c
田中冬紀 <f.tanaka@gftd.works>
2022-05-27 11:05:38 +0900

    Revert "bootstrapに対応させました"
    This reverts commit af1b82c50e09050c907cdb0ea98e85f4be86a638.

--- assessment.js
+++ assessment.js
@@ -24,28 +24,14 @@ assessmentButton.onclick = () => {
 
     // 診断結果表示エリアの作成
     removeAllChildren(resultDivided);
-    // headerDivided の作成
-  　　　　const headerDivided = document.createElement('div');
-  　　　　headerDivided.setAttribute('class', 'card-header');
-  　　　　headerDivided.innerText = '診断結果';
+    const header = document.createElement('h3');
+    header.innerText = '診断結果';
+    resultDivided.appendChild(header);
 
-  　　　　// bodyDivided の作成
-  　　　　const bodyDivided = document.createElement('div');
-  　　　　bodyDivided.setAttribute('class', 'card-body');
-
-  　　　　const paragraph = document.createElement('p');
-  　　　　paragraph.setAttribute('class', 'card-text');
-  　　　　const result = assessment(userName);
-  　　　　paragraph.innerText = result;
-  　　　　bodyDivided.appendChild(paragraph);
-
-  　　　　// resultDivided に Bootstrap のスタイルを適用する
-  　　　　resultDivided.setAttribute('class', 'card');
-  　　　　resultDivided.setAttribute('style', 'max-width: 700px;')
-
-  　　　　// headerDivided と bodyDivided を resultDivided に差し込む
-  　　　　resultDivided.appendChild(headerDivided);
-  　　　　resultDivided.appendChild(bodyDivided);
+    const paragraph = document.createElement('p');
+    const result = assessment(userName);
+    paragraph.innerText = result;
+    resultDivided.appendChild(paragraph);
 
     // ツイートエリアの作成
     removeAllChildren(tweetDivided);
